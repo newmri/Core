c54999ae2a69c5ccc44469f1c5e76b44cda80e6a
newmri <newmri@naver.com>
2021-07-12 21:36:21 +0900

    CoreParallelSTL 파일 분리

--- Source/Core/Core/CoreParallelSTL.h
+++ Source/Core/Core/CoreParallelSTL.h
@@ -5,43 +5,26 @@ class CoreParallelSTL
 {
 public:
 	CoreParallelSTL() CORE_DEFAULT;
-	CoreParallelSTL(const size_t newCapacity) : capacity(newCapacity) {}
+	CoreParallelSTL(const size_t newCapacity);
 	virtual ~CoreParallelSTL() CORE_DEFAULT;
 
 public:
 	virtual void Push(const T newData) CORE_PURE;
+
 protected:
-	virtual void Push(void) { SetSize(this->size + 1); }
+	virtual void Push(void);
 
 public:
-	virtual void Pop(void) { SetSize(this->size - 1); };
-	virtual void Clear(void) { SetSize(0); };
+	virtual void Pop(void);
+	virtual void Clear(void);
 
 public:
-	size_t GetCapacity(void)
-	{
-		READ_LOCK(this->mutex);
-		return this->capacity; 
-	}
-
-	size_t GetSize(void)
-	{
-		READ_LOCK(this->mutex);
-		return this->size; 
-	}
+	size_t GetCapacity(void);
+	size_t GetSize(void);
 
 private:
-	void SetSize(const size_t newSize)
-	{
-		WRITE_LOCK(mutex);
-		this->size = newSize;
-	}
-
-	void SetCapacity(const size_t newCapacity)
-	{
-		WRITE_LOCK(mutex);
-		this->capacity = newCapacity;
-	}
+	void SetSize(const size_t newSize);
+	void SetCapacity(const size_t newCapacity);
 
 protected:
 	size_t capacity = 0;
@@ -50,3 +33,5 @@ protected:
 protected:
 	std::shared_mutex mutex;
 };
+
+#include "CoreParallelSTL.h"
