140d4ca27cd0944c3de166b6f0a729833f59a9ab
newmri <newmri@naver.com>
2021-07-27 23:09:23 +0900

    랜덤에 시간 값 추가, 가중치에 따라 하나 뽑아주는 함수 추가

--- Source/Core/Core/CoreRandomManager.h
+++ Source/Core/Core/CoreRandomManager.h
@@ -13,6 +13,7 @@ public:
 	bool GetRandomByPercent(const float percent = 0.5f);
 	int GetRandom(const int from, const int to);
 	size_t GetRandom(const size_t from, const size_t to);
+	size_t GetRandom(const CoreVector<size_t>& percentList);
 	float GetRandom(const float from, const float to);
 	double GetRandom(const double from, const double to);
 };
