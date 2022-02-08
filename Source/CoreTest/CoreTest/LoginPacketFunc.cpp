d10dffdf3f3464c2949d2a536b3d502e92ed919b
newmri <newmri@naver.com>
2022-02-09 00:56:40 +0900

    지역 builder 남은거 삭제

--- Source/CoreTest/CoreTest/LoginPacketFunc.cpp
+++ Source/CoreTest/CoreTest/LoginPacketFunc.cpp
@@ -12,8 +12,6 @@ void LoginPacketFunc::SC_LOGIN_RES(CoreServerSession& session, const void* data)
 	auto raw = static_cast<const Login::SC_LOGIN_RES*>(data);
 	CORE_LOG.Log(TO_STR(raw->result()));
 
-	flatbuffers::FlatBufferBuilder builder;
-
 	auto name = builder.CreateString("Hello World!");
 	auto message = Login::CreateCS_LOGIN_REQ(this->builder, name);
 	Write(session, Login::Packet_CS_LOGIN_REQ, message.Union());
