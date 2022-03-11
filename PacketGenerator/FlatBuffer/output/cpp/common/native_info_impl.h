c9dbe1059d5c91615fda83cb1c5f316813c2455b
newmri <newmri@naver.com>
2022-03-12 02:54:39 +0900

    커밋 누락 추가

--- PacketGenerator/FlatBuffer/output/cpp/common/native_info_impl.h
+++ PacketGenerator/FlatBuffer/output/cpp/common/native_info_impl.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "define_protocol_generated.h"
+
 namespace NativeInfo
 {
     struct Vec2Int
@@ -23,12 +25,12 @@ namespace NativeInfo
         {
         }
 
-        Stat(const int16_t* stats)
+        Stat(const flatbuffers::Array<int32_t, Define::StatType_END>* stats)
         {
-            memcpy_s(this->value, sizeof(this->value), stats, sizeof(this->value));
+            std::copy(stats->begin(), stats->end(), this->value.begin());
         }
 
-        int16_t value[Define::StatType_END];
+        std::array<int32_t, Define::StatType_END> value;
     };
 
     struct CharacterGear
@@ -37,12 +39,12 @@ namespace NativeInfo
         {
         }
 
-        CharacterGear(const uint8_t* gears)
+        CharacterGear(const flatbuffers::Array<uint8_t, Define::GearType_END>* index)
         {
-            this->index = gears;
+            std::copy(index->begin(), index->end(), this->index.begin());
         }
 
-        CoreArray<uint8_t, Define::GearType_END> index;
+        std::array<uint8_t, Define::GearType_END> index;
     };
 }  
 
@@ -56,11 +58,11 @@ namespace Info
 namespace flatbuffers
 {
     Info::Vec2Int PackVec2Int(const NativeInfo::Vec2Int& obj);
-    const NativeInfo::Vec2Int UnPackVec2Int(const Info::Vec2Int& obj);
+    NativeInfo::Vec2Int UnPackVec2Int(const Info::Vec2Int& obj);
 
     Info::Stat PackStat(const NativeInfo::Stat& obj);
-    const NativeInfo::Stat UnPackStat(const Info::Stat& obj);
+    NativeInfo::Stat UnPackStat(const Info::Stat& obj);
 
     Info::CharacterGear PackCharacterGear(const NativeInfo::CharacterGear& obj);
-    const NativeInfo::CharacterGear UnPackCharacterGear(const Info::CharacterGear& obj);
+    NativeInfo::CharacterGear UnPackCharacterGear(const Info::CharacterGear& obj);
 }
