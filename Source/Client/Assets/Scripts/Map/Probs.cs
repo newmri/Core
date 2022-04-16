2c1c04302ee102b395b78e97df03688b04e8b465
newmri <newmri@naver.com>
2022-04-16 12:36:49 +0900

    맵 크기 확장, 여러 오브젝트 추가

---
+++ Source/Client/Assets/Scripts/Map/Probs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Probs : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.position = new Vector3(0.0f, -0.5f, 0.0f);
+    }
+}
