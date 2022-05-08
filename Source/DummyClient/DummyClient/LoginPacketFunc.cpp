8273972482356893e9f395c3215869a35a779049
newmri <newmri@naver.com>
2022-05-08 17:30:45 +0900

    DummyClient 빌드 가능하게 수정

--- Source/DummyClient/DummyClient/LoginPacketFunc.cpp
+++ Source/DummyClient/DummyClient/LoginPacketFunc.cpp
@@ -1,1 +1,16 @@
 #include "Include.h"
+
+void LoginPacketFunc::SC_LOGIN_RES(CoreServerSession& session, const void* data)
+{
+}
+
+void LoginPacketFunc::SC_PING_REQ(CoreServerSession& session, const void* data)
+{
+	PACKET_SEND_MANAGER.builder.Clear();
+	auto message = LoginPacket::CreateCS_PING_RES(PACKET_SEND_MANAGER.builder);
+	PACKET_SEND_MANAGER.Send(session, LoginPacket::Packet_CS_PING_RES, message.Union());
+}
+
+void LoginPacketFunc::SC_CREATE_CHARACTER_RES(CoreServerSession& session, const void* data)
+{
+}
--- Source/DummyClient/DummyClient/LoginPacketFunc.h
+++ Source/DummyClient/DummyClient/LoginPacketFunc.h
@@ -3,4 +3,7 @@
 class LoginPacketFunc
 {
 public:
+	void SC_LOGIN_RES(CoreServerSession& session, const void* data);
+	void SC_PING_REQ(CoreServerSession& session, const void* data);
+	void SC_CREATE_CHARACTER_RES(CoreServerSession& session, const void* data);
 };
