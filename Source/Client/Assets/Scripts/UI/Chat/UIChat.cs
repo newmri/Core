3796f28d3ff906d352a646d686557555b04dbaf3
newmri <newmri@naver.com>
2022-07-08 00:32:56 +0900

    채팅 ui 80%완료

--- Source/Client/Assets/Scripts/UI/Chat/UIChat.cs
+++ Source/Client/Assets/Scripts/UI/Chat/UIChat.cs
@@ -3,18 +3,62 @@ using System.Collections.Generic;
 using TMPro;
 using UnityCoreLibrary;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ChatType
+{
+    NORMAL,
+    PARTY,
+    GUILD,
+    WHISPER,
+    SYSTEM,
+    MAX
+}
 
 public class UIChat : UIBase
 {
-    enum InputTexts
+    enum GameObjects
     {
-        InputText
+        ChatContent,
+        ChatList
     }
 
+    enum Buttons
+    {
+        ChatTypeButton
+    }
+
+    enum TextMeshProUGUIs
+    {
+        ChatTypeText
+    }
+
+    enum InputTexts
+    {
+        ChatInputText
+    }
+
+    [SerializeField]
+    private Sprite[] _chatInputTypeSprite;
+
+    [SerializeField]
+    private string[] _chatInputTypeName;
+
+    [SerializeField]
+    private Color[] _textColorList;
+
+    private ChatType _currInputType;
+    private Color _currTextColor;
     public override void Init()
     {
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(TextMeshProUGUIs));
         Bind<TMP_InputField>(typeof(InputTexts));
+
         gameObject.SetActive(false);
+        _currInputType = ChatType.NORMAL;
+        _currTextColor = Color.black;
     }
 
     public void OnUpdate()
@@ -24,22 +68,51 @@ public class UIChat : UIBase
 
     public bool IsFocused()
     {
-        return this.GetInputText((int)InputTexts.InputText).isFocused;
+        return this.GetInputText((int)InputTexts.ChatInputText).isFocused;
     }
 
     private void GetUIKeyInput()
     {
         if (Input.GetKeyDown(KeyCode.Return))
+            UpdateChatText();
+        else if (Input.GetKeyDown(KeyCode.Tab) && IsFocused())
+            UpdateChatInputType();
+    }
+
+    private void UpdateChatText()
+    {
+        if (gameObject.activeSelf)
         {
-            if (gameObject.activeSelf)
+            if (this.GetInputText((int)InputTexts.ChatInputText).text.Length > 0)
             {
-                gameObject.SetActive(false);
+                GameObject go = CoreManagers.Resource.Instantiate($"UI/UIChatText", GetObject((int)GameObjects.ChatContent).transform, 20);
+                UIChatData chatData = go.GetComponent<UIChatData>();
+                chatData.SetChatData(_currInputType, _currTextColor, $"{this.GetInputText((int)InputTexts.ChatInputText).text}");
+
+                this.GetInputText((int)InputTexts.ChatInputText).text = "";
             }
-            else
-            {
-                gameObject.SetActive(true);
-                this.GetInputText((int)InputTexts.InputText).ActivateInputField();
-            }
+
+            gameObject.SetActive(false);
         }
+        else
+        {
+            gameObject.SetActive(true);
+            this.GetInputText((int)InputTexts.ChatInputText).ActivateInputField();
+            GetObject((int)GameObjects.ChatList).GetComponent<ScrollRect>().verticalNormalizedPosition = -1;
+        }
+    }
+
+    private void UpdateChatInputType()
+    {
+        _currInputType = (int)_currInputType < (int)ChatType.MAX - 3 ? _currInputType + 1 : 0;
+        OnChangedChatInputType();
+    }
+
+    private void OnChangedChatInputType()
+    {
+        GetButton((int)Buttons.ChatTypeButton).GetComponent<Image>().sprite = _chatInputTypeSprite[(int)_currInputType];
+        this.GetTextMesh((int)TextMeshProUGUIs.ChatTypeText).text = _chatInputTypeName[(int)_currInputType];
+
+        _currTextColor = _textColorList[(int)_currInputType];
     }
 }
---
+++ Source/Client/Assets/Scripts/UI/Chat/UIChatData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityCoreLibrary;
+using UnityEngine;
+
+public class UIChatData : UIBase
+{
+    public ChatType ChatType;
+    
+    public override void Init()
+    {
+
+    }
+
+    public void SetChatData(ChatType chatType, Color color, string text)
+    {
+        ChatType = chatType;
+
+        TextMeshProUGUI textUGUI = GetComponent<TextMeshProUGUI>();
+        textUGUI.color = color;
+        textUGUI.text = text;
+    }
+
+}
